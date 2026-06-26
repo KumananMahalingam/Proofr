84c985b0ea9a88735a27e7b7d14c99c5836a86f7
KumananMahalingam <mkumanan@outlook.com>
2026-06-25 21:51:21 -0400

    fixed transaprent UI component

--- components/confirm-modal.tsx
+++ components/confirm-modal.tsx
@@ -36,20 +36,23 @@ export const ConfirmModal = ({
       <AlertDialogTrigger asChild>
         {children}
       </AlertDialogTrigger>
-      <AlertDialogContent>
+      <AlertDialogContent className="bg-white text-neutral-900 border border-neutral-200">
         <AlertDialogHeader>
-          <AlertDialogTitle>
+          <AlertDialogTitle className="text-neutral-900">
             {header}
           </AlertDialogTitle>
-          <AlertDialogDescription>
+          <AlertDialogDescription className="text-neutral-500">
             {description}
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogCancel className="bg-white text-neutral-700 border-neutral-300 hover:bg-neutral-100">
+            Cancel
+          </AlertDialogCancel>
           <AlertDialogAction
             disabled={disabled}
             onClick={handleConfirm}
+            className="bg-blue-600 text-white hover:bg-blue-700"
           >
             Confirm
           </AlertDialogAction>
