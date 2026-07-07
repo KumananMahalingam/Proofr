cbdd42309e2c2a0a42c517f9e1a2f0ba49d7f5c7
KumananMahalingam <mkumanan@outlook.com>
2026-07-06 22:45:13 -0400

    updated organization naming

--- app/(dashboard)/_components/sidebar/new-button.tsx
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -16,7 +16,7 @@ export const NewButton = () => {
       <DialogTrigger asChild>
         <div className="aspect-square">
           <Hint
-            label="Create organization"
+            label="Create workspace"
             side="right"
             align="start"
             sideOffset={18}
